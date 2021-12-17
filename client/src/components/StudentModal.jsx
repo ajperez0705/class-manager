27e6894d2c8a1848589dee77f0bb32f91a8440cc
Andrew Perez <57570632+ajperez0705@users.noreply.github.com>
2021-12-16 23:52:41 -0500

    Added to the student carts

--- client/src/components/StudentModal.jsx
+++ client/src/components/StudentModal.jsx
@@ -4,6 +4,7 @@ import decode from "jwt-decode";
 
 function StudentModal({ student, modalStatus, setModalStatus }) {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const [feedBack, setFeedBack] = useState("positive");
   const [isTeacher, setIsTeacher] = useState(null);
   console.log(student);
 
@@ -28,34 +29,44 @@ function StudentModal({ student, modalStatus, setModalStatus }) {
               src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
               wrapped
             />
-            <Modal.Description>
-              <div className="toggle-header">
-                <Header>Positive Feedback</Header>
-                <Header>Negative Feedback</Header>
-              </div>
-              <Card>
-                <Image
-                  src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
-                  wrapped
-                  ui={false}
-                />
-                <Card.Content>
-                  <Card.Header>Helping Others</Card.Header>
-                </Card.Content>
-              </Card>
-            </Modal.Description>
+
+            {feedBack === "positive" ? (
+              <Modal.Description>
+                <div className="toggle-header">
+                  <Header>Positive Feedback</Header>
+                  <Header onClick={() => setFeedBack("negative")}>
+                    Negative Feedback
+                  </Header>
+                </div>
+                <Card>
+                  <Icon name="thumbs up outline" size="huge" />
+                  <Card.Content>
+                    <Card.Header>Helping Others</Card.Header>
+                  </Card.Content>
+                </Card>
+              </Modal.Description>
+            ) : (
+              <Modal.Description>
+                <div className="toggle-header">
+                  <Header onClick={() => setFeedBack("positive")}>
+                    Positive Feedback
+                  </Header>
+                  <Header>Negative Feedback</Header>
+                </div>
+                <Card>
+                  <Icon name="thumbs down outline" size="huge" />
+                  <Card.Content>
+                    <Card.Header>Disrespecting Others</Card.Header>
+                  </Card.Content>
+                </Card>
+              </Modal.Description>
+            )}
           </Modal.Content>
+
           <Modal.Actions>
             <Button color="black" onClick={() => setModalStatus(false)}>
-              Nope
+              Close
             </Button>
-            <Button
-              content="Yep, that's me"
-              labelPosition="right"
-              icon="checkmark"
-              onClick={() => setModalStatus(false)}
-              positive
-            />
           </Modal.Actions>
         </>
       ) : (
@@ -70,20 +81,15 @@ function StudentModal({ student, modalStatus, setModalStatus }) {
               wrapped
             />
             <Modal.Description>
-              <div className="toggle-header">
-                <Header>Positive Feedback</Header>
-                <Header>Negative Feedback</Header>
-              </div>
-              <Card>
-                <Image
-                  src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
-                  wrapped
-                  ui={false}
-                />
-                <Card.Content>
-                  <Card.Header>Helping Others</Card.Header>
-                </Card.Content>
-              </Card>
+              <Header>Points</Header>
+              <Card meta="Positive Points" description={student.posPoints} />
+              <Card meta="Negativ Points" description={student.negPoints} />
+              <Card meta="Total Points" description={student.totalPoints} />
+            </Modal.Description>
+
+            <Modal.Description>
+              <Header>Trophies</Header>
+              <Card meta="Trophies" description={student.totalTrophies} />
             </Modal.Description>
           </Modal.Content>
           <Modal.Actions>
